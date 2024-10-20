f64fcf43cc02145228b28f41ca62ec5320f73297
pya-h <p4ya.gcc@gmail.com>
2024-10-20 04:32:55 +0330

    add extra names for username generation & fix some bugs - Prevent the app to crash when the admin token is wrong, now app tries to register users normally if the admin importing is failed due to any reason. - Add near than 20000 names to the identity.json file to extend the generation name ranges. - Prettifying app.js code a little.

--- app.js
+++ app.js
@@ -7,19 +7,16 @@ import BotConfig from "./config.js";
 
 const importBots = async (count) => {
     const conf = BotConfig.Get();
+    const botCredentials = await generateBotsImportData({
+        count,
+        password: conf.botPassword,
+        maxBotLevel: conf.botMaxLevel,
+        saltRounds: conf.hashSaltRound,
+        privateProfile: conf.isBotProfilePrivate,
+        forRegister: false,
+    });
     try {
-        const botCredentials = await generateBotsImportData({
-            count,
-            password: conf.botPassword,
-            maxBotLevel: conf.botMaxLevel,
-            saltRounds: conf.hashSaltRound,
-            privateProfile: conf.isBotProfilePrivate,
-            forRegister: false,
-        });
-
-        const { status, data, message } = await ApiService.Get().import(
-            botCredentials
-        );
+        const { status, data, message } = await ApiService.Get().import(botCredentials);
         if (status !== 201) throw new Error(message);
 
         const { successCount, failures } = data;
@@ -32,14 +29,11 @@ const importBots = async (count) => {
             );
             await saveJsonData("import_failures", failures);
         }
-
-        return botCredentials?.map(
-            (botIdentity) =>
-                new Bot({ ...botIdentity, password: conf.botPassword })
-        );
     } catch (ex) {
         botlog.x("admin", "fail to import bots, since:", ex);
     }
+
+    return botCredentials?.map((botIdentity) => new Bot({ ...botIdentity, password: conf.botPassword }));
 };
 
 const loadBotsPreviousLeagues = async (bots) => {
@@ -53,9 +47,7 @@ const manageBotsParticipation = async (bots) => {
 };
 
 const manageBotsPlaying = async (bots) => {
-    const insignificantTasks = await Promise.all(
-        bots.map((bot) => bot.tryPlaying())
-    );
+    const insignificantTasks = await Promise.all(bots.map((bot) => bot.tryPlaying()));
     await Promise.all(insignificantTasks.flat());
 };
 
@@ -63,17 +55,13 @@ const setup = async () => {
     const conf = BotConfig.Get();
     let bots = conf.loadLastState ? await Bot.LoadState() : [];
     if (conf.botsCount > 0) {
-        botlog.w(
-            "admin",
-            `importing ${conf.botsCount} bots, wait a little ...`
-        );
+        botlog.w("admin", `importing ${conf.botsCount} bots, wait a little ...`);
         const newBots = await importBots(conf.botsCount);
         bots.push(...newBots);
         await saveJsonData("total_bots", bots);
     }
 
-    if (!bots?.length)
-        throw new Error("Could not prepare any bot. App will close now.");
+    if (!bots?.length) throw new Error("Could not prepare any bot. App will close now.");
 
     await Bot.ForceLoginBots(bots);
 
@@ -83,21 +71,13 @@ const setup = async () => {
         await Bot.ForceLoginBots(bots);
     });
 
-    cron.schedule(
-        `*/${+conf.botParticipationIntervalInMinutes} * * * *`,
-        async () => {
-            await manageBotsParticipation(bots);
-        }
-    );
+    cron.schedule(`*/${+conf.botParticipationIntervalInMinutes} * * * *`, async () => {
+        await manageBotsParticipation(bots);
+    });
 
-    cron.schedule(
-        `*/${+conf.botGameplayIntervalInSeconds} * * * * *`,
-        async () => {
-            await manageBotsPlaying(bots);
-        }
-    );
+    cron.schedule(`*/${+conf.botGameplayIntervalInSeconds} * * * * *`, async () => {
+        await manageBotsPlaying(bots);
+    });
 };
 
-setup().catch((err) =>
-    botlog.x("manager", "Bot manager failed to setup.", err)
-);
+setup().catch((err) => botlog.x("manager", "Bot manager failed to setup.", err));
